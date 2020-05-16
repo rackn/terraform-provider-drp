733a6c7a47c7e4fdb4eaedf566bdada27c82c22c
Filippo Projetto <fili.projetto@gmail.com>
2020-05-16 10:15:39 +0100

    go 14

--- tools/build.sh
+++ tools/build.sh
@@ -9,7 +9,7 @@ fi
 
 # Work out the GO version we are working with:
 GO_VERSION=$(go version | awk '{ print $3 }' | sed 's/go//')
-WANTED_VER=(1 12)
+WANTED_VER=(1 14)
 if ! [[ "$GO_VERSION" =~ ([0-9]+)\.([0-9]+) ]]; then
     echo "Cannot figure out what version of Go is installed"
     exit 1
